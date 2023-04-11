fd939f6ef6c71b96c3d813495e985b090dba4b08
Frank <f.neumann.berlin@googlemail.com>
2023-04-11 09:28:59 +0200

    mit User-class und userList

--- app/src/main/java/de/softdeveloper/jsondemo/MainActivity.kt
+++ app/src/main/java/de/softdeveloper/jsondemo/MainActivity.kt
@@ -2,10 +2,34 @@ package de.softdeveloper.jsondemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.material.tabs.TabLayout.TabGravity
+import de.softdeveloper.jsondemo.databinding.ActivityMainBinding
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val TAG = this.javaClass.simpleName
+    private var userList: ArrayList<User> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val inputStream = assets.open("users.json")
+        // auf alter Java Basis wie folgt zu verwenden
+//        val isr = InputStreamReader(inputStream)
+//        val br = BufferedReader(isr)
+//        val jsonString = br.readText()
+        // so geht es aktuell
+        val jsonString = inputStream.bufferedReader().use {bufferedReader -> 
+            bufferedReader.readText()
+        }
+        Log.d(TAG, "onCreate: JsonString -> $jsonString")
+        
+        
     }
 }
